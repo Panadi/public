d6e4779d517d642ddcdd0e55e89ae390264bbad5
Italo <italo300r@gmail.com>
2023-05-12 12:03:00 -0300

    Adição de nivel

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -1,12 +1,32 @@
-import random   
+import random 
+import sys  
 print("**************")
 print("Bem vindo ao teste")
 print("**************")
-tentativas = 1
-erro1 = 0
+
+
 numero_aleatorio = random.randint(1, 10)
 
-while(tentativas <= 4):
+erro1 = 0
+tentativas = 1
+nv_max = 3
+nv_min = 1
+try:
+   nivel = int(input("Escolha o nivel de 1 à 3:"))
+   if(nivel > nv_max or nivel < nv_min):
+    print("Digite um numero entre o intervalo indicado.")
+    sys.exit()
+except ValueError:
+   print("Por favor digite um numero inteiro.")
+   sys.exit()
+if(nivel == 1):
+   total_tentativas = 5
+elif(nivel == 2):
+    total_tentativas = 3
+else:
+    total_tentativas = 1
+
+while(tentativas <= total_tentativas):
     try:
         chute = int(input("Essa é a {} tentativa digite seu chute:".format(tentativas)))
     except ValueError:
