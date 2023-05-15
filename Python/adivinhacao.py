623e04a8e69cf8696c09f012f0eb60d40a859c9e
Italo <italo300r@gmail.com>
2023-05-15 16:54:06 -0300

    Adição de def e pontuação

--- Python/adivinhacao.py
+++ Python/adivinhacao.py
@@ -6,45 +6,56 @@ print("**************")
 
 
 numero_aleatorio = random.randint(1, 10)
-
 erro1 = 0
-tentativas = 1
 nv_max = 3
 nv_min = 1
-try:
-   nivel = int(input("Escolha o nivel de 1 à 3:"))
-   if(nivel > nv_max or nivel < nv_min):
-    print("Digite um numero entre o intervalo indicado.")
-    sys.exit()
-except ValueError:
-   print("Por favor digite um numero inteiro.")
-   sys.exit()
-if(nivel == 1):
-   total_tentativas = 5
-elif(nivel == 2):
-    total_tentativas = 3
-else:
-    total_tentativas = 1
+pontuacao = 1000
 
-while(tentativas <= total_tentativas):
+def adivinhacao(total_tentativas, pontuacao):
+    tentativas = 1
+    while(tentativas <= total_tentativas):
+        erro1 = 0
+        try:
+            chute = int(input("Essa é a {} tentativa digite seu chute:".format(tentativas)))
+        except ValueError:
+            erro1 = 1
+            print("Por favor digite um numero inteiro.") 
+        acerto = chute == numero_aleatorio 
+        maior = chute > numero_aleatorio 
+        menor = chute < numero_aleatorio 
+    
+    
+        if(erro1 == 1):
+         print()
+        else:
+         if(acerto):
+          print(f"Acertou!!na {tentativas}° tentativa.")
+          break
+         elif(maior):
+           print("Errou! você escolheu um numero maior.")
+           pontuacao -= 10
+         elif(menor):
+           print("Errou! você escolheu um numero menor.")
+           pontuacao -= 10
+        tentativas += 1
+        return pontuacao
+def nivel():
     try:
-        chute = int(input("Essa é a {} tentativa digite seu chute:".format(tentativas)))
+        nivel = int(input("Escolha o nivel de 1 à 3:"))
+        if(nivel > nv_max or nivel < nv_min):
+            print("Digite um numero entre o intervalo indicado.")
+            sys.exit()
     except ValueError:
-       erro1 = 1
-       print("Por favor digite um numero inteiro.") 
-    acerto = chute == numero_aleatorio 
-    maior = chute > numero_aleatorio 
-    menor = chute < numero_aleatorio 
+        print("Por favor digite um numero inteiro.")
+        sys.exit()
+    return nivel
+   
+def chances(nivel):
+   total_tentativas = int((5/nivel)*2)
+   return total_tentativas
 
-    if(erro1 == 1):
-     print()
-    else:
-        if(acerto):
-         print(f"Acertou!!na {tentativas}° tentativa")
-         break
-        elif(maior):
-           print("Errou! você escolheu um numero maior")
-        elif(menor):
-           print("Errou! você escolheu um numero menor")
-    tentativas += 1
+total_tentativas = chances(nivel())
+adivinhacao(total_tentativas,pontuacao)
+pontuacao_final = adivinhacao(total_tentativas, pontuacao)
+print("Sua pontuação final é: ", pontuacao_final)
 print("FIM")
