6935277b87f76ffa1e8d41a826c137f8b677e2a4
Italo <italo300r@gmail.com>
2023-05-19 16:56:59 -0300

    forca

--- Python/forca.py
+++ Python/forca.py
@@ -1,11 +1,28 @@
 # forca.py
 
+import sys
+
 def jogar():
 
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+    
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+        print("Jogando...")
+        while True:
+         chute = input("Digite uma letra: ")
+         if len(chute) != 1 or not chute.isalpha():
+          print("Digite apenas uma letra!")
+          sys.exit()
+         
 
     print("Fim do jogo")
+
+
 if (__name__ == "__main__"):
    jogar()
