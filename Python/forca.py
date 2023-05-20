db0a7d9b5cdac44680e85589b6ce83572fc3e3bf
Italo <italo300r@gmail.com>
2023-05-20 10:48:48 -0300

    jogo

--- Python/forca.py
+++ Python/forca.py
@@ -23,6 +23,44 @@ def jogar():
 
     print("Fim do jogo")
 
+#def jogar_forca():
+    palavra_secreta = input("Digite a palavra secreta: ").lower()
+    chances = 6
+    letras_acertadas = []
+    letras_erradas = []
+
+    while True:
+        letra = input("Digite uma letra: ").lower()
+
+        if letra in letras_acertadas or letra in letras_erradas:
+            print("Você já tentou essa letra. Tente outra.")
+            continue
+
+        if letra in palavra_secreta:
+            letras_acertadas.append(letra)
+        else:
+            letras_erradas.append(letra)
+            chances -= 1
+
+        print("Palavra: ", end="")
+        for caractere in palavra_secreta:
+            if caractere in letras_acertadas:
+                print(caractere, end=" ")
+            else:
+                print("_", end=" ")
+        print("\n")
+
+        print("Letras erradas:", letras_erradas)
+        print("Chances restantes:", chances)
+        print("\n")
+
+        if len(letras_acertadas) == len(set(palavra_secreta)):
+            print("Parabéns! Você acertou a palavra secreta:", palavra_secreta)
+            break
+
+        if chances == 0:
+            print("Você perdeu! A palavra secreta era:", palavra_secreta)
+            break
 
 if (__name__ == "__main__"):
    jogar()
