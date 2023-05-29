0f53217d3277371161a4b7e8724b696cefb7981f
Italo <italo300r@gmail.com>
2023-05-29 16:50:39 -0300

    O fim no jogo da forca

--- Python/forca.py
+++ Python/forca.py
@@ -11,56 +11,29 @@ def jogar():
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
-
+    letras_acertadas = ["_"] * len(palavra_secreta)
+    letras_erradas = []
+    print(letras_acertadas)
     while(not acertou and not enforcou):
         print("Jogando...")
-        while True:
-         chute = input("Digite uma letra: ")
-         if len(chute) != 1 or not chute.isalpha():
+        
+        chute = input("Digite uma letra: ")
+
+        if (len(chute) != 1 or not chute.isalpha()):
           print("Digite apenas uma letra!")
-          sys.exit()
-         
-
-    print("Fim do jogo")
-
-#def jogar_forca():
-    palavra_secreta = input("Digite a palavra secreta: ").lower()
-    chances = 6
-    letras_acertadas = []
-    letras_erradas = []
-
-    while True:
-        letra = input("Digite uma letra: ").lower()
-
-        if letra in letras_acertadas or letra in letras_erradas:
-            print("Você já tentou essa letra. Tente outra.")
-            continue
-
-        if letra in palavra_secreta:
-            letras_acertadas.append(letra)
+          break
         else:
-            letras_erradas.append(letra)
-            chances -= 1
-
-        print("Palavra: ", end="")
-        for caractere in palavra_secreta:
-            if caractere in letras_acertadas:
-                print(caractere, end=" ")
-            else:
-                print("_", end=" ")
-        print("\n")
-
-        print("Letras erradas:", letras_erradas)
-        print("Chances restantes:", chances)
-        print("\n")
-
-        if len(letras_acertadas) == len(set(palavra_secreta)):
-            print("Parabéns! Você acertou a palavra secreta:", palavra_secreta)
-            break
-
-        if chances == 0:
-            print("Você perdeu! A palavra secreta era:", palavra_secreta)
-            break
+          index = 0
+          for letra in palavra_secreta:
+              if (chute.upper == letra.upper):
+                letras_acertadas[index] = letra
+                print(f"Acertou a letra {chute}, na posição {index}")
+              index += 1           
+          print(letras_acertadas) 
+          if "_" not in letras_acertadas:
+                acertou = True
+                print("Você descobriu a palavra!!!!")         
+    print("Fim do jogo")
 
 if (__name__ == "__main__"):
    jogar()
